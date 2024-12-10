76e64b199d2793aaaaf69b36be7002b387570564
Kaushik <kaushik19231@gmail.com>
2024-12-10 11:10:00 +0530

    feat(media): implement imagekit authentication parameters endpoint

---
+++ app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,24 @@
+import { getUploadAuthParams } from "@imagekit/next/server";
+
+export async function GET() {
+  try {
+    const { token, expire, signature } = getUploadAuthParams({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+    });
+
+    return Response.json({
+      token,
+      expire,
+      signature,
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        error: "Authentication for imagekit failed",
+      },
+      { status: 500 }
+    );
+  }
+}
